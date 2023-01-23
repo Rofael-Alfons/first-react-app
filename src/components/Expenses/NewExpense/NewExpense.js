60c7d16692c50520c204584823b5e04749a0b430
Rofael <rofa.alfons7@gmail.com>
2023-01-23 08:50:45 +0200

    another commit

--- src/components/Expenses/NewExpense/NewExpense.js
+++ src/components/Expenses/NewExpense/NewExpense.js
@@ -2,9 +2,15 @@ import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = () => {
+  const saveExpenseDataHandler = function (data) {
+    const expenseData = {
+      ...data,
+    };
+    console.log(expenseData);
+  };
   return (
     <div className="new-expense">
-      <ExpenseForm></ExpenseForm>
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}></ExpenseForm>
     </div>
   );
 };
